a4f1b2ec2061e965c608e71dd54a1ad0cacc2c8b
Karthik <karthik2502@protonmail.com>
2024-11-25 22:39:51 +0530

    continuing with reviewing the part-1 section modifying js

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -132,3 +132,25 @@ lines"
 console.log(`String
 multiple
 lines`);
+
+///////////////////////////
+const age = 15;
+
+if (age >= 18) {
+  console.log("Sarah can start driving license 🚗");
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+
+const birthYear = 2012;
+
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century);
+
+//////////////////////////
