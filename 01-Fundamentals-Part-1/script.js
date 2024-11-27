52c8227f65530191340ced09e34c0ada4af3c132
Karthik <karthik2502@protonmail.com>
2024-11-27 21:55:41 +0530

    continuing with reviewing the part-1 section modifying js

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -154,3 +154,18 @@ if (birthYear <= 2000) {
 console.log(century);
 
 //////////////////////////
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+if (BMIMark > BMIJohn) {
+  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+} else {
+  console.log(`John's BMI (${BMIJohn}) is higher than Marks's (${BMIMark})!`);
+}
