6d6d583cb590a2c22af272f433ba9a510a2fbd12
Karthik <karthik2502@protonmail.com>
2024-11-29 22:20:00 +0530

    continuing with reviewing the part-1 section modifying js

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -322,3 +322,18 @@ const me = "Jonas";
 console.log(`I'm ${2037 - 1991} years old ${me}`);
 
 ////////////////////////////////////
+const age = 23;
+
+const drink = age >= 18 ? "wine 🍷" : "water 💧";
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+  drink2 = "wine 🍷";
+} else {
+  drink2 = "water 💧";
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? "wine 🍷" : "water 💧"}`);
+////////////////////////////////////
