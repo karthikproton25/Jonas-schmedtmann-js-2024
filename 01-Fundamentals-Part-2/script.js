27705bc67daee81011b20ed146f9b9a00bfec8fd
Karthik <karthik2502@protonmail.com>
2025-03-02 23:47:09 +0530

    Section 3 - lesson 34

--- 01-Fundamentals-Part-2/script.js
+++ 01-Fundamentals-Part-2/script.js
@@ -9,23 +9,35 @@
 // const interface = "Audio";
 // const private = 534;
 
-function logger() {
-  console.log("My name is Jonas");
+// function logger() {
+//   console.log("My name is Jonas");
+// }
+
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
+
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+// const num = Number("23");
+
+function calcAge1(birthYeah) {
+  return 2037 - birthYeah;
 }
+const age1 = calcAge1(1991);
 
-logger();
-logger();
-logger();
+const calcAge2 = function (birthYeah) {
+  return 2037 - birthYeah;
+};
+const age2 = calcAge2(1991);
 
-function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-  return juice;
-}
-
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
-
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
-
-const num = Number("23");
+console.log(age1, age2);
