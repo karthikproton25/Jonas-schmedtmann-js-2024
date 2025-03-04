82989de36727e2bb05c0b16c69b7f887fe974690
Karthik <karthik2502@protonmail.com>
2025-03-04 22:47:48 +0530

    Section 3 - lesson 35

--- 01-Fundamentals-Part-2/script.js
+++ 01-Fundamentals-Part-2/script.js
@@ -30,14 +30,28 @@
 
 // const num = Number("23");
 
-function calcAge1(birthYeah) {
-  return 2037 - birthYeah;
-}
-const age1 = calcAge1(1991);
+// function calcAge1(birthYeah) {
+//   return 2037 - birthYeah;
+// }
+// const age1 = calcAge1(1991);
 
-const calcAge2 = function (birthYeah) {
-  return 2037 - birthYeah;
+// const calcAge2 = function (birthYeah) {
+//   return 2037 - birthYeah;
+// };
+// const age2 = calcAge2(1991);
+
+// console.log(age1, age2);
+
+const calcAge3 = (birthYeah) => 2037 - birthYeah;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYeah, firstName) => {
+  const age = 2037 - birthYeah;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
 };
-const age2 = calcAge2(1991);
 
-console.log(age1, age2);
+console.log(yearsUntilRetirement(1991, "Jonas"));
+console.log(yearsUntilRetirement(1980, "Bob"));
