1a74920a7fe4bd976eb45b2134ef4a09314120b8
Karthik <karthik2502@protonmail.com>
2025-03-05 21:57:03 +0530

    Section 3 - adding comments

--- 01-Fundamentals-Part-2/script.js
+++ 01-Fundamentals-Part-2/script.js
@@ -1,5 +1,5 @@
 "use strict";
-
+// Activating Strict Mode
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -9,10 +9,12 @@ if (hasDriversLicense) console.log("I can drive :D");
 const interface = "Audio";
 const private = 534;
 
+// Functions
 function logger() {
   console.log("My name is Jonas");
 }
 
+// calling / running / invoking function
 logger();
 logger();
 logger();
@@ -30,11 +32,13 @@ console.log(appleOrangeJuice);
 
 const num = Number("23");
 
+// Function declaration
 function calcAge1(birthYeah) {
   return 2037 - birthYeah;
 }
 const age1 = calcAge1(1991);
 
+// Function expression
 const calcAge2 = function (birthYeah) {
   return 2037 - birthYeah;
 };
@@ -42,6 +46,7 @@ const age2 = calcAge2(1991);
 
 console.log(age1, age2);
 
+// Arrow functions
 const calcAge3 = (birthYeah) => 2037 - birthYeah;
 const age3 = calcAge3(1991);
 console.log(age3);
@@ -49,13 +54,13 @@ console.log(age3);
 const yearsUntilRetirement = (birthYeah, firstName) => {
   const age = 2037 - birthYeah;
   const retirement = 65 - age;
-  // return retirement;
   return `${firstName} retires in ${retirement} years`;
 };
 
 console.log(yearsUntilRetirement(1991, "Jonas"));
 console.log(yearsUntilRetirement(1980, "Bob"));
 
+// Functions Calling Other Functions
 function cutFruitPieces(fruit) {
   return fruit * 4;
 }
