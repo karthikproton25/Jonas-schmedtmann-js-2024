fc3a8f4ecf217fceef3fe43933ebca2a6e785787
Karthik <karthik2502@protonmail.com>
2025-03-06 20:02:06 +0530

    Section 3 - lesson 40

--- 01-Fundamentals-Part-2/script.js
+++ 01-Fundamentals-Part-2/script.js
@@ -134,3 +134,12 @@ console.log(friends[2]);
 
 console.log(friends.length);
 console.log(friends[friends.length - 1]);
+
+friends[2] = "Jay";
+console.log(friends);
+friends = ["Bob", "Alice"];
+
+const firstName = "Jonas";
+const jonas = [firstName, "Schmedtmann", 2037 - 1991, "teacher", friends];
+console.log(jonas);
+console.log(jonas.length);
