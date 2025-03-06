4365f8af9892915b566c2f69d95863ea4ade875e
Karthik <karthik2502@protonmail.com>
2025-03-06 20:01:29 +0530

    Section 3 - lesson 40

--- 01-Fundamentals-Part-2/script.js
+++ 01-Fundamentals-Part-2/script.js
@@ -118,3 +118,19 @@ const checkWinner = function (avgDolphins, avgKoalas) {
 };
 checkWinner(scoreDolphins, scoreKoalas);
 checkWinner(576, 111);
+
+// Introduction to Arrays
+const friend1 = "Michael";
+const friend2 = "Steven";
+const friend3 = "Peter";
+
+const friends = ["Michael", "Steven", "Peter"];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
