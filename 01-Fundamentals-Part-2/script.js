5331d646cfbaefcae22ffd91ac864aebf0d58fcc
Karthik <karthik2502@protonmail.com>
2025-03-18 22:38:19 +0530

    solution for the coding chanllenge - lesson 41

--- 01-Fundamentals-Part-2/script.js
+++ 01-Fundamentals-Part-2/script.js
@@ -192,3 +192,14 @@ console.log(friends.includes(23));
 if (friends.includes("Steven")) {
   console.log("You have a friend called Steven");
 }
+
+// Coding challenge 2
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bils = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills, tips, totals);
