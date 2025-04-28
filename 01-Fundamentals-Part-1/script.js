9f481c268480de552bab583e2b016e469a311c76
Karthik <karthik2502@protonmail.com>
2025-04-28 21:37:07 +0530

    lesson 42 - basic array operations

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -309,3 +309,32 @@ const ages = [
   calcAge(years[years.length - 1]),
 ];
 console.log(ages);
+////////////////////////////////////
+const friends = ["Michael", "Steven", "Peter"];
+
+const newLength = friends.push("Jay");
+console.log(friends);
+console.log(newLength);
+
+friends.unshift("John");
+console.log(friends);
+
+friends.pop();
+const popped = friends.pop();
+console.log(popped);
+console.log(friends);
+
+friends.shift();
+console.log(friends);
+
+console.log(friends.indexOf("Steven"));
+console.log(friends.indexOf("Bob"));
+
+friends.push(23);
+console.log(friends.includes("Steven"));
+console.log(friends.includes("Bob"));
+console.log(friends.includes(23));
+
+if (friends.includes("Steven")) {
+  console.log("You have a friend called Steven");
+}
