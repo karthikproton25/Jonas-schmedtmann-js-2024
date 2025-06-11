34fe2653714155841b56753162b0bd5e5bbb0bf5
Karthik <karthik2502@protonmail.com>
2025-06-11 07:45:51 +0530

    lesson 41 - basic array operations

--- 01-Fundamentals-Part-2/script.js
+++ 01-Fundamentals-Part-2/script.js
@@ -160,3 +160,14 @@ const ages = [
   calcAge(years[years.length - 1]),
 ];
 console.log(ages);
+
+// Basic Array Operations (Methods)
+const friends = ['Michael', 'Steven', 'Peter'];
+
+// Add elements
+const newLength = friends.push('Jay');
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('John');
+console.log(friends);
