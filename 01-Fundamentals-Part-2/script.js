212a10826c13f828dd533de0fac64c59ff98adbf
Karthik <karthik2502@protonmail.com>
2025-06-16 20:37:53 +0530

    redoing the object methods lesson

--- 01-Fundamentals-Part-2/script.js
+++ 01-Fundamentals-Part-2/script.js
@@ -239,8 +239,6 @@ const nameKey = "Name";
 console.log(jonas["first" + nameKey]);
 console.log(jonas["last" + nameKey]);
 
-// console.log(jonas.'last' + nameKey)
-
 const interestedIn = prompt(
   "What do you want to know about Jonas? Choose between firstName, lastName, age, job, and friends"
 );
@@ -256,40 +254,3 @@ if (jonas[interestedIn]) {
 jonas.location = "Portugal";
 jonas["twitter"] = "@jonasschmedtman";
 console.log(jonas);
-
-// Object Methods
-
-const jonas = {
-  firstName: "Jonas",
-  lastName: "Schmedtmann",
-  birthYeah: 1991,
-  job: "teacher",
-  friends: ["Michael", "Peter", "Steven"],
-  hasDriversLicense: true,
-
-  // calcAge: function (birthYeah) {
-  //   return 2037 - birthYeah;
-  // }
-
-  // calcAge: function () {
-  //   // console.log(this);
-  //   return 2037 - this.birthYeah;
-  // }
-
-  calcAge: function () {
-    this.age = 2037 - this.birthYeah;
-    return this.age;
-  },
-
-  getSummary: function () {
-    return `${this.firstName} is a ${this.calcAge()}-year old ${
-      jonas.job
-    }, and he has ${this.hasDriversLicense ? "a" : "no"} driver's license.`;
-  },
-};
-
-console.log(jonas.calcAge());
-
-console.log(jonas.age);
-console.log(jonas.age);
-console.log(jonas.age);
