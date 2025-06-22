56932af849cf4d4740431edc36bb14e6db70c1de
Karthik <karthik2502@protonmail.com>
2025-06-22 16:24:27 +0530

    lesson 45 - Object methods part-2

--- 01-Fundamentals-Part-2/script.js
+++ 01-Fundamentals-Part-2/script.js
@@ -267,4 +267,10 @@ const jonas = {
   calcAge: function (birthYeah) {
     return 2037 - birthYeah;
   },
+
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()}-year old ${
+      jonas.job
+    }, and he has ${this.hasDriversLicense ? "a" : "no"} driver's license.`;
+  },
 };
