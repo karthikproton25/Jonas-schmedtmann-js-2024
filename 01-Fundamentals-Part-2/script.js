ae91ca79cbd0e2d23a9c44aa6740eb17f3fa99bf
Karthik <karthik2502@protonmail.com>
2025-06-28 23:26:20 +0530

    lesson 48 - part 1

--- 01-Fundamentals-Part-2/script.js
+++ 01-Fundamentals-Part-2/script.js
@@ -311,8 +311,26 @@ if (mark.bmi > john.bmi) {
   );
 }
 
-// Iteration: The for Loop
+// Iteration for Loop
 
 for (let rep = 1; rep <= 30; rep++) {
   console.log(`Lifting weights repetition ${rep} 🏋️‍♀️`);
 }
+
+// Looping Arrays, Breaking and Continuing
+const jonas = [
+  "Jonas",
+  "Schmedtmann",
+  2037 - 1991,
+  "teacher",
+  ["Michael", "Peter", "Steven"],
+  true,
+];
+const types = [];
+
+for (let i = 0; i < jonas.length; i++) {
+  console.log(jonas[i], typeof jonas[i]);
+  types.push(typeof jonas[i]);
+}
+
+console.log(types);
